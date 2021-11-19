71b1e4577a8533a0234d4d53f9e4b18319005f15
Deperius <jhonk1999@gmail.com>
2021-11-19 12:29:28 -0500

    CRUD products completo

--- src/main/java/com/guarderia/products_ms/controllers/ProductController.java
+++ src/main/java/com/guarderia/products_ms/controllers/ProductController.java
@@ -3,11 +3,14 @@ package com.guarderia.products_ms.controllers;
 import com.guarderia.products_ms.exceptions.ProductNotFoundException;
 import com.guarderia.products_ms.models.Product;
 import com.guarderia.products_ms.repositories.ProductsRepository;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
-    private final ProductsRepository productsRepository;
+    private ProductsRepository productsRepository;
 
     public ProductController(ProductsRepository productsRepository) {
         this.productsRepository = productsRepository;
@@ -19,8 +22,35 @@ public class ProductController {
                 .orElseThrow(() -> new ProductNotFoundException("No se encontro el producto con id: " + id));
     }
 
+    @GetMapping("/products/")
+    List<Product> getAllProducts(){
+        return productsRepository.findAll();
+    }
+
     @PostMapping("/product")
     Product newProduct(@RequestBody Product product){
         return productsRepository.save(product);
     }
+
+    @PutMapping("/product/{id}")
+    Product modifyProduct(@RequestBody Product product, @PathVariable String id){
+        Product mio = productsRepository.findById(id)
+                .orElse(null);
+        if (mio == null)
+            throw  new ProductNotFoundException("No se encontro el producto con id: " + id);
+
+        product.setId(id);
+        return productsRepository.save(product);
+    }
+
+    @DeleteMapping("product/{id}")
+    void deleteProduct(@PathVariable String id){
+        Product mio = productsRepository.findById(id)
+                        .orElse(null);
+        if (mio == null)
+            throw  new ProductNotFoundException("No se encontro el producto con id: " + id);
+
+        productsRepository.deleteById(id);
+    }
+
 }
